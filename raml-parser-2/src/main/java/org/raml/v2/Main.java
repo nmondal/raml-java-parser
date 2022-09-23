6e8bcf5e63020d4fa283543194137c110334a46f
noga <nabarun.mondal@gmail.com>
2022-09-23 23:58:56 +0530

    Adding raml and started playing with types.

---
+++ raml-parser-2/src/main/java/org/raml/v2/Main.java
@@ -0,0 +1,33 @@
+package org.raml.v2;
+
+import org.raml.v2.api.RamlModelBuilder;
+import org.raml.v2.api.RamlModelResult;
+import org.raml.v2.api.model.common.ValidationResult;
+import org.raml.v2.api.model.v10.api.Api;
+import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
+
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args){
+        final File ramlFile = new File("raml-parser-2/models/types.raml");
+        RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(ramlFile);
+        if (ramlModelResult.hasErrors())
+        {
+            for (ValidationResult validationResult : ramlModelResult.getValidationResults())
+            {
+                System.out.println(validationResult.getMessage());
+            }
+        }
+        else
+        {
+            Api api = ramlModelResult.getApiV10();
+            List<TypeDeclaration> types = Objects.requireNonNull(api).types();
+            for ( TypeDeclaration td : types){
+                System.out.println( td.name() );
+            }
+        }
+    }
+}
